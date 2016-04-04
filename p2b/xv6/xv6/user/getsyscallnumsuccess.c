595bcefe35a07fc7261d1d0d591cfc14e08da783
Richard Lee <rlee23@wisc.edu>
2016-04-03 23:11:17 -0500

    Deleted P2

--- p2b/xv6/xv6/user/getsyscallnumsuccess.c
+++
@@ -1,9 +0,0 @@
-#include "types.h"
-#include "user.h"
-
-int
-main(int argc, char *argv[])
-{
-  printf(1, "Number of Successful Syscalls: %d\n", getsyscallnumsuccess());
-  exit();
-}
--- p2b/xv6/xv6/user/getsyscallnumtotal.c
+++
@@ -1,9 +0,0 @@
-#include "types.h"
-#include "user.h"
-
-int
-main(int argc, char *argv[])
-{
-  printf(1, "Total Number of Syscalls: %d\n", getsyscallnumtotal());
-  exit();
-}
